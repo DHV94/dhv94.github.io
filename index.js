87f9552c0077d7383f32d1663aec9f3bdcd197d2
DeMarco <127225564+DeMrreti@users.noreply.github.com>
2023-03-17 05:48:17 +0100

    v4.1

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-numero = 12
-
-if (12 > 20) {
-    numero = 20
-}
-
 function siguienteDiapositiva() {
-    alert('Invadidos por los gais')
+    document.getElementById("caja").style.display = "none"
 }
