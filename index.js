83bab6777b33e64582f23afde7b5d48c47a44107
DeMarco <127225564+DeMrreti@users.noreply.github.com>
2023-03-21 01:00:25 +0100

    v5.3

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 function siguienteDiapositiva1() {
-    document.getElementById("caja").style.zIndex = 0
-    document.getElementById("caja2").style.zIndex = 1
+    document.getElementById("caja2").style.display = 'flex'
 }
 
 function siguienteDiapositiva2() {
-    document.getElementById("caja2").style.zIndex = 0
-    document.getElementById("caja3").style.zIndex = 1
+    document.getElementById("caja3").style.display = 'flex'
 }
 
 function siguienteDiapositiva3() {
-    document.getElementById("caja3").style.zIndex = 0
-    document.getElementById("caja4").style.zIndex = 1
+    document.getElementById("caja4").style.display = 'flex'
 }
 
 function anteriorDiapositiva2() {
-    document.getElementById("caja").style.zIndex = 1
-    document.getElementById("caja2").style.zIndex = 0
+    document.getElementById("caja2").style.display = 'none'
 }
 
 function anteriorDiapositiva3() {
-    document.getElementById("caja2").style.zIndex = 1
-    document.getElementById("caja3").style.zIndex = 0
+    document.getElementById("caja3").style.display = 'none'
 }
 
 function anteriorDiapositiva4() {
-    document.getElementById("caja3").style.zIndex = 1
-    document.getElementById("caja4").style.zIndex = 0
+    document.getElementById("caja4").style.display = 'none'
 }
 
 
