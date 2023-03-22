b244c2144b95bc1faf342ff9d7421a9abaaed3d7
DeMarco <127225564+DeMrreti@users.noreply.github.com>
2023-03-22 19:44:54 +0100

    Revert "Merge branch 'main' of https://github.com/DHV94/dhv94.github.io"
    This reverts commit 414e5e2e3a3ef22574f29ae6a899098fe339656d, reversing
    changes made to 54c46c4aacea3ae676d7872e7cfc8b09f0e6a75b.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function siguienteDiapositiva1() {
-    document.getElementById("caja2").style.display = "flex";
+    document.getElementById("caja2").style.display = 'flex'
 }
 
 function siguienteDiapositiva2() {
-    document.getElementById("caja3").style.display = "flex";
+    document.getElementById("caja3").style.display = 'flex'
 }
 
 function siguienteDiapositiva3() {
-    document.getElementById("caja4").style.display = "flex";
+    document.getElementById("caja4").style.display = 'flex'
 }
 
 function anteriorDiapositiva2() {
-    document.getElementById("caja2").style.display = "none";
+    document.getElementById("caja2").style.display = 'none'
 }
 
 function anteriorDiapositiva3() {
-    document.getElementById("caja3").style.display = "none";
+    document.getElementById("caja3").style.display = 'none'
 }
 
 function anteriorDiapositiva4() {
-    document.getElementById("caja4").style.display = "none";
+    document.getElementById("caja4").style.display = 'none'
 }
 
 
